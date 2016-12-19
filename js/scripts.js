6a1741a44ee15159142f70940af0e9fb9d06cc0b
Amy Hwang <amyhwang1218@gmail.com>
2016-12-19 15:55:15 -0800

    finish styling home

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// $(function() {
+//   $(".mobile-menu-btn").on('click', function() {
+//     $("body").toggleClass('js-navbar-open');
+//   });
+//
+  // var num = 200;
+//
+//   $(window).bind('scroll', function () {
+//     if ($(window).scrollTop() > num) {
+//       $('.nav-bar-wrapper').addClass('fixed');
+//     } else {
+//       $('.nav-bar-wrapper').removeClass('fixed');
+//     }
+//   });
+// });
