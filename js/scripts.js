c3712db449dea0dfd5a37f36d660f23357bd3971
Amy Hwang <amyhwang1218@gmail.com>
2016-12-20 12:17:13 -0800

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
-// $(function() {
-//   $(".mobile-menu-btn").on('click', function() {
-//     $("body").toggleClass('js-navbar-open');
-//   });
-//
-  // var num = 200;
-//
-//   $(window).bind('scroll', function () {
-//     if ($(window).scrollTop() > num) {
-//       $('.nav-bar-wrapper').addClass('fixed');
-//     } else {
-//       $('.nav-bar-wrapper').removeClass('fixed');
-//     }
-//   });
-// });
+$(function() {
+  // $(".mobile-menu-btn").on('click', function() {
+  //   $("body").toggleClass('js-navbar-open');
+  // });
+  //
+  // var num = $('.container').offset().top + $('.container').height();
+  //
+  // $(window).bind('scroll', function () {
+  //   if ($(window).scrollTop() > num) {
+  //     $('.main-nav').addClass('fixed');
+  //   } else {
+  //     $('.main-nav').removeClass('fixed');
+  //   }
+  // });
+    function isScrolledTo(elem) {
+      var docViewTop = $(window).scrollTop(); //num of pixels hidden above current screen
+      var docViewBottom = docViewTop + $(window).height();
+      var elemTop = $(elem).offset().top; //num of pixels above the elem
+      var elemBottom = elemTop + $(elem).height();
+      return ((elemTop <= docViewTop));
+    }
+    var catcher = 0;
+    var sticky = $('.main-nav');
+    $(window).scroll(function() {
+        if(isScrolledTo(sticky)) {
+            sticky.css('position','fixed');
+            sticky.css('top','0px');
+        }
+	       var stopHeight = catcher;
+	        if ( stopHeight > sticky.offset().top) {
+            sticky.css('position','static');
+            sticky.css('top', 0);
+        }
+    });
+
+});
